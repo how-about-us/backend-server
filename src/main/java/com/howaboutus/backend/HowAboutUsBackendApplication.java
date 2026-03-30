6b4b79f6efe6fb2fd0e851585beda422892e8a09
minbros <realminbros@gmail.com>
2026-03-31 02:26:56 +0900

    Initial Commit

---
+++ src/main/java/com/howaboutus/backend/HowAboutUsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.howaboutus.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowAboutUsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HowAboutUsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/howaboutus/backend/HowAboutUsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.howaboutus.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowAboutUsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

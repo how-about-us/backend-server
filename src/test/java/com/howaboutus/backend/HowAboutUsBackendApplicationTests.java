86282a1d77407d3741582d0b11996b6d76f41cf0
minbros <realminbros@gmail.com>
2026-04-10 15:21:26 +0900

    test: PostgreSQL 테스트컨테이너 공통 베이스 추가

--- src/test/java/com/howaboutus/backend/HowAboutUsBackendApplicationTests.java
+++ src/test/java/com/howaboutus/backend/HowAboutUsBackendApplicationTests.java
@@ -1,10 +1,11 @@
 package com.howaboutus.backend;
 
+import com.howaboutus.backend.support.AbstractPostgresContainerTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HowAboutUsBackendApplicationTests {
+class HowAboutUsBackendApplicationTests extends AbstractPostgresContainerTest {
 
     @Test
     void contextLoads() {

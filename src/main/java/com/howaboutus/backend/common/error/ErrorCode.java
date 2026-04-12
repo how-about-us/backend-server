644c882ce7e8a496eee19c8d375108fe2e4c68ee
minbros <realminbros@gmail.com>
2026-04-12 22:04:14 +0900

    refactor: 추가 리팩토링

--- src/main/java/com/howaboutus/backend/common/error/ErrorCode.java
+++ src/main/java/com/howaboutus/backend/common/error/ErrorCode.java
@@ -9,7 +9,10 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     // 400 BAD REQUEST
-    INVALID_PLACE_QUERY(HttpStatus.BAD_REQUEST, "검색어는 공백일 수 없습니다");
+    INVALID_PLACE_QUERY(HttpStatus.BAD_REQUEST, "검색어는 공백일 수 없습니다"),
+
+    // 502 BAD GATEWAY
+    EXTERNAL_API_ERROR(HttpStatus.BAD_GATEWAY, "외부 API 호출 중 오류가 발생했습니다");
 
     private final HttpStatus status;
     private final String message;

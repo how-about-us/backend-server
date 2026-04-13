81ed8bc36c96f833606cedbb141ba4729ea779e3
parkjuyeong0312 <dcmo77777@gmail.com>
2026-04-13 16:02:16 +0900

    feat: 인증 관련 ErrorCode 추가

--- src/main/java/com/howaboutus/backend/common/error/ErrorCode.java
+++ src/main/java/com/howaboutus/backend/common/error/ErrorCode.java
@@ -11,6 +11,10 @@ public enum ErrorCode {
     // 400 BAD REQUEST
     INVALID_PLACE_QUERY(HttpStatus.BAD_REQUEST, "검색어는 공백일 수 없습니다"),
 
+    // 401 UNAUTHORIZED
+    GOOGLE_AUTH_FAILED(HttpStatus.UNAUTHORIZED, "Google 인증에 실패했습니다"),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다"),
+
     // 502 BAD GATEWAY
     EXTERNAL_API_ERROR(HttpStatus.BAD_GATEWAY, "외부 API 호출 중 오류가 발생했습니다");
 

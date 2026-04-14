87f88ff04205c900222b592843d3e38db67c067e
parkjuyeong0312 <dcmo77777@gmail.com>
2026-04-14 10:38:53 +0900

    fix : access token 만료시간, application.yml에서 가져오게 수정

--- src/main/java/com/howaboutus/backend/auth/controller/AuthController.java
+++ src/main/java/com/howaboutus/backend/auth/controller/AuthController.java
@@ -3,6 +3,8 @@ package com.howaboutus.backend.auth.controller;
 import com.howaboutus.backend.auth.controller.dto.GoogleLoginRequest;
 import com.howaboutus.backend.auth.service.AuthService;
 import java.time.Duration;
+
+import com.howaboutus.backend.common.config.properties.JwtProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
@@ -18,16 +20,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+    private final JwtProperties jwtProperties;
 
     @PostMapping("/google/login")
     public ResponseEntity<Void> googleLogin(@RequestBody GoogleLoginRequest request) {
         String accessToken = authService.googleLogin(request.code());
-
+        //구글로그인 시도시, 요청 헤더에, Authentication code를 받아와서, google에 로그인요청을 목척으로
         ResponseCookie cookie = ResponseCookie.from("access_token", accessToken)
                 .httpOnly(true)
                 .sameSite("Lax")
                 .path("/")
-                .maxAge(Duration.ofHours(1))
+                .maxAge(Duration.ofMillis(jwtProperties.accessTokenExpiration()))
                 .build();
 
         return ResponseEntity.ok()

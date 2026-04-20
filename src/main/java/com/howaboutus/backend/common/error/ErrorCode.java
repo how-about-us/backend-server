66eec7a0e70053ef69ab33bf85311788cfcbcd96
parkjuyeong0312 <dcmo77777@gmail.com>
2026-04-20 13:53:33 +0900

    feat: ACCESS_TOKEN_EXPIRED, USER_NOT_FOUND 에러 코드 추가

--- src/main/java/com/howaboutus/backend/common/error/ErrorCode.java
+++ src/main/java/com/howaboutus/backend/common/error/ErrorCode.java
@@ -11,6 +11,7 @@ public enum ErrorCode {
     // 401 UNAUTHORIZED
     GOOGLE_AUTH_FAILED(HttpStatus.UNAUTHORIZED, "Google 인증에 실패했습니다"),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다"),
+    ACCESS_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "액세스 토큰이 만료되었습니다"),
     REFRESH_TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "리프레시 토큰이 존재하지 않습니다"),
     REFRESH_TOKEN_REUSE_DETECTED(HttpStatus.UNAUTHORIZED, "토큰 재사용이 감지되었습니다"),
 
@@ -23,6 +24,9 @@ public enum ErrorCode {
     BOOKMARK_CATEGORY_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 북마크 카테고리입니다"),
     BOOKMARK_CATEGORY_EMPTY(HttpStatus.CONFLICT, "북마크 카테고리가 없습니다"),
     BOOKMARK_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 보관함에 추가된 장소입니다"),
+
+    // 404 NOT FOUND
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다"),
 
     // 502 BAD GATEWAY
     EXTERNAL_API_ERROR(HttpStatus.BAD_GATEWAY, "외부 API 호출 중 오류가 발생했습니다");

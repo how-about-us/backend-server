b8bb9550c5753da3a26b11c708343f84dbe3c190
parkjuyeong0312 <dcmo77777@gmail.com>
2026-04-20 22:17:33 +0900

    feat: Room 관련 ErrorCode 추가 (INVALID_DATE_RANGE, NOT_ROOM_MEMBER, NOT_ROOM_HOST, USER_NOT_FOUND)

--- src/main/java/com/howaboutus/backend/common/error/ErrorCode.java
+++ src/main/java/com/howaboutus/backend/common/error/ErrorCode.java
@@ -8,6 +8,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
+    // 400 BAD REQUEST
+    INVALID_DATE_RANGE(HttpStatus.BAD_REQUEST, "시작일이 종료일보다 늦을 수 없습니다"),
+
     // 401 UNAUTHORIZED
     GOOGLE_AUTH_FAILED(HttpStatus.UNAUTHORIZED, "Google 인증에 실패했습니다"),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다"),
@@ -15,8 +18,13 @@ public enum ErrorCode {
     REFRESH_TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "리프레시 토큰이 존재하지 않습니다"),
     REFRESH_TOKEN_REUSE_DETECTED(HttpStatus.UNAUTHORIZED, "토큰 재사용이 감지되었습니다"),
 
+    // 403 FORBIDDEN
+    NOT_ROOM_MEMBER(HttpStatus.FORBIDDEN, "방의 멤버가 아닙니다"),
+    NOT_ROOM_HOST(HttpStatus.FORBIDDEN, "호스트 권한이 필요합니다"),
+
     // 404 NOT FOUND
-    ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "방을 찾을 수 없습니다"),
+    ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 방입니다"),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다"),
     BOOKMARK_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "북마크 카테고리를 찾을 수 없습니다"),
     BOOKMARK_NOT_FOUND(HttpStatus.NOT_FOUND, "북마크를 찾을 수 없습니다"),
 

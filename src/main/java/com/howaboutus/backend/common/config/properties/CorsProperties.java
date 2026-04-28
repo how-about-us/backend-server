76269eff00e7ae24ea770087d8c45b97a2a8c3db
minbros <realminbros@gmail.com>
2026-04-28 14:49:31 +0900

    fix: 배포 서버에서 CORS가 null이어서 발생하는 에러 해결

--- src/main/java/com/howaboutus/backend/common/config/properties/CorsProperties.java
+++ src/main/java/com/howaboutus/backend/common/config/properties/CorsProperties.java
@@ -1,10 +1,17 @@
 package com.howaboutus.backend.common.config.properties;
 
 import java.util.List;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "cors")
 public record CorsProperties(
         List<String> allowedOrigins
 ) {
+    public CorsProperties {
+        if (allowedOrigins == null) {
+            allowedOrigins = List.of();
+        }
+        allowedOrigins = List.copyOf(allowedOrigins);
+    }
 }
